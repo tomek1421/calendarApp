f32406715129e167f08a3e65997fd0ad0e4147c2
tomek_1421 <t.wojciechowski.648@studms.ug.edu.pl>
2024-03-18 16:26:44 +0100

    add registration and login routes

--- calendar-typescript/src/App.tsx
+++ calendar-typescript/src/App.tsx
@@ -3,13 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { Home } from './components/Home';
 import { Error } from './components/Error';
+import { Register } from './components/Register';
+import { Login } from './components/Login';
+import { SharedLayout } from './components/SharedLayout';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Error />} />
+        <Route path="/" element={<SharedLayout />} >
+          <Route index element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
