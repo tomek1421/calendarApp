29ff8bdeac827378269c225ea49ca585b224bec9
tomek_1421 <t.wojciechowski.648@studms.ug.edu.pl>
2024-04-05 12:13:04 +0200

    add admin page

---
+++ calendar-typescript/src/pages/AdminPanel.tsx
@@ -0,0 +1,10 @@
+import Raect from 'react'
+import { CreateSubject } from '../components/CreateSubject'
+
+export const AdminPanel: React.FC = () => {
+    return (
+        <div>
+            <CreateSubject />
+        </div>
+    )
+}
